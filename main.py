58444d3e04351a16f64aecb20d186e61dd4a587f
Aditya Kumar <aaz22hsns@gmail.com>
2023-06-28 16:21:34 +0530

    complete basic functionalities

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    joke = ""
+    if request.method == "POST":
+        api_endpoint = "https://v2.jokeapi.dev/joke/Any?type=single"
+        api_response = requests.get(api_endpoint).json()
+        joke = api_response["joke"]
+        print(joke)
+        return render_template('index.html',joke=joke)
+
+    return render_template('index.html', joke=joke)
+
 
 if __name__ == "__main__":
     app.run()
